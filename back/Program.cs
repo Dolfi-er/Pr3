25b445487303e704d0b027678999d30075eab44c
Dolfi-er <dolgovasofia@yandex.ru>
2025-12-12 18:01:08 +0300

    DonkiCME

--- back/Program.cs
+++ back/Program.cs
@@ -44,6 +44,7 @@ builder.Services.AddScoped<ISpaceCacheRepository, SpaceCacheRepository>();
 builder.Services.AddHostedService<IssBackgroundService>();
 builder.Services.AddHostedService<ApodBackgroundService>();
 builder.Services.AddHostedService<NeoBackgroundService>();
+builder.Services.AddHostedService<DonkiCMEBackgroundService>();
 builder.Services.AddHttpClient<IssApiService>();
 
 var app = builder.Build();
