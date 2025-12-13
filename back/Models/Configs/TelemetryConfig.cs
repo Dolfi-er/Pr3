5757aa6f1c3efc6bdefca07b8461992ffb6847ce
Dolfi-er <dolgovasofia@yandex.ru>
2025-12-13 17:08:28 +0300

    Telemetry Models

---
+++ back/Models/Configs/TelemetryConfig.cs
@@ -0,0 +1,34 @@
+using back.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace back.Models.Configs;
+
+public class TelemetryConfig : IEntityTypeConfiguration<TelemetryEntity>
+{
+    public void Configure(EntityTypeBuilder<TelemetryEntity> entity)
+    {
+        entity.ToTable("telemetry");
+
+        entity.HasKey(e => e.Id);
+
+        entity.Property(e => e.Timestamp)
+            .HasColumnName("timestamp")
+            .HasColumnType("timestamptz"); 
+
+        entity.Property(e => e.IsOk)
+            .HasColumnName("is_ok");
+
+        entity.Property(e => e.Voltage)
+            .HasColumnName("voltage")
+            .HasColumnType("double precision");
+
+        entity.Property(e => e.Temp)
+            .HasColumnName("temp")
+            .HasColumnType("double precision");
+
+        entity.Property(e => e.SourceFile)
+            .HasColumnName("source_file")
+            .HasColumnType("text");
+    }
+}
---
+++ back/Models/Entities/Telemetry.cs
@@ -0,0 +1,11 @@
+namespace back.Models.Entities;
+
+public class TelemetryEntity
+{
+    public int Id { get; set; } 
+    public DateTime Timestamp { get; set; }
+    public bool IsOk { get; set; }
+    public double Voltage { get; set; }
+    public double Temp { get; set; }
+    public string SourceFile { get; set; } = string.Empty;
+}
